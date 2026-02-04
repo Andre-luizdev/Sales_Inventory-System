3073657ee71e7fd2b4f32aa158ad21fdfccff27c
Andre Luiz <andre-developer@outlook.com>
2026-02-03 22:34:01 -0300

    add exception handling for user input using try-catch

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,7 +3,7 @@ package application;
 
 import entities.Inventory;
 import entities.Product;
-import exception.ErrorException;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,58 +22,65 @@ public class Program {
 
         int op = 1;
         while (op != 0) {
-
+            try {
                 System.out.println();
                 System.out.print("1-Cadastrar | 2-Add Estoque | 3-Remover | 4-Ver Tudo | 0-Sair: ");
                 op = sc.nextInt();
                 sc.nextLine();
                 System.out.println();
 
-            if (op == 1) {
-                System.out.print("Digite o nome do novo produto: ");
-                String name = sc.nextLine();
-                System.out.print("Digite a quantidade: ");
-                int qnt = sc.nextInt();
-                System.out.print("Digite o valor $ - ");
-                double price = sc.nextDouble();
-                Product product = new Product(name, price, qnt);
-                inventory.addProductToInventory(product);
-            } else if (op == 2) {
+                if (op == 1) {
+                    System.out.print("Digite o nome do novo produto: ");
+                    String name = sc.nextLine();
+                    System.out.print("Digite a quantidade: ");
+                    int qnt = sc.nextInt();
+                    System.out.print("Digite o valor $ - ");
+                    double price = sc.nextDouble();
+                    Product product = new Product(name, price, qnt);
+                    inventory.addProductToInventory(product);
+                } else if (op == 2) {
 
-                System.out.print("Informe do produto: ");
-                String busca = sc.nextLine();
-                Product p = inventory.findProduct(busca);
-                if (p != null) {
-                    System.out.print("Quanto adicionar? ");
-                    int addQnt = sc.nextInt();
-                    p.addProduct(addQnt);
-                    System.out.println("Atualizado: " + p);
+                    System.out.print("Informe do produto: ");
+                    String busca = sc.nextLine();
+                    Product p = inventory.findProduct(busca);
+                    if (p != null) {
+                        System.out.print("Quanto adicionar? ");
+                        int addQnt = sc.nextInt();
+                        p.addProduct(addQnt);
+                        System.out.println("Atualizado: " + p);
+                    } else {
+                        System.out.println("Produto não encontrado!");
+                    }
+                } else if (op == 3) {
+                    System.out.println();
+                    System.out.print("Informe o produto: ");
+                    String busca = sc.nextLine();
+                    Product p = inventory.findProduct(busca);
+                    if (p != null) {
+                        System.out.print("Quanto a remover? ");
+                        int removeQnt = sc.nextInt();
+                        p.removeProduct(removeQnt);
+                        System.out.println("Atualizado: " + p);
+                    } else {
+                        System.out.println("Erro: Saldo insuficiente!");
+                    }
                 } else {
                     System.out.println("Produto não encontrado!");
+                    System.out.println();
                 }
-            } else if (op == 3) {
-                System.out.println();
-                System.out.print("Informe o produto: ");
-                String busca = sc.nextLine();
-                Product p = inventory.findProduct(busca);
-                if (p != null) {
-                    System.out.print("Quanto a remover? ");
-                    int removeQnt = sc.nextInt();
-                    p.removeProduct(removeQnt);
-                    System.out.println("Atualizado: " + p);
-                } else {
-                    System.out.println("Erro: Saldo insuficiente!");
+                if (op == 4) {
+                    System.out.println("-----ESTOQUE ATUAL-----");
+                    inventory.showInventory();
+                } else if (op != 0) {
+                    System.out.println("Opção inválida!");
                 }
-            } else {
-                System.out.println("Produto não encontrado!");
+            } catch (java.util.InputMismatchException e){
                 System.out.println();
+                System.out.println("ERRO: Você digitou um valor inválido!");
+                sc.nextLine();
+                op = -1;
             }
-            if (op == 4) {
-                System.out.println("-----ESTOQUE ATUAL-----");
-                inventory.showInventory();
-            } else if (op != 0) {
-                System.out.println("Opção inválida!");
-            }
+
         }
         System.out.println("Sistema finalizado!");
 
--- src/exception/ErrorException.java
+++
@@ -1,7 +0,0 @@
-package exception;
-
-public class ErrorException extends RuntimeException {
-    public ErrorException(String message) {
-        super(message);
-    }
-}
